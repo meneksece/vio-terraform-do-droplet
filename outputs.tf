a013518045f2cc877e76fbc02477a4d6a06edf30
violetadelamancha <mnksecyln@gmail.com>
2023-10-10 11:06:11 +0200

    yeni folder

--- outputs.tf
+++
@@ -1,7 +0,0 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MIT
-
-output "ip_address" {
-  value = digitalocean_droplet.web.ipv4_address
-  description = "The public IP address of your droplet application."
-}
